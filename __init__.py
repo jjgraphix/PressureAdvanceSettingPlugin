6295240fc020ad60e3e98972f3262fbc345fc3eb
JJFX <jjfx.contact@gmail.com>
2022-05-23 19:57:22 -0700

    Update copyrights

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
-# Personal Use Only - JJFX 2020
-# PressureAdvanceSettingPlugin Based on LinearAdvanceSettingPlugin (Copyright (c) 2020 Aldo Hoeben / fieldOfView)
+# For Personal Use - Copyright (c) 2022 JJFX Design
+# Project based on LinearAdvanceSettingPlugin (Copyright (c) 2020 Aldo Hoeben / fieldOfView)
+# The PressureAdvanceSettingPlugin is released under the terms of the AGPLv3 or higher.
 
 from . import PressureAdvanceSettingPlugin
 
